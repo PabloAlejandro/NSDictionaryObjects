6a3c1929ee307433376c10626ce8e61af36d9d54
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2016-04-28 15:59:27 +0100

    Initial Commit

---
+++ NSDictionaryObjects/NSDictionaryObjects.h
@@ -0,0 +1,19 @@
+//
+//  NSDictionaryObjects.h
+//  NSDictionaryObjects
+//
+//  Created by Pau on 28/04/2016.
+//  Copyright © 2016 Pablo Alejandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSDictionaryObjects.
+FOUNDATION_EXPORT double NSDictionaryObjectsVersionNumber;
+
+//! Project version string for NSDictionaryObjects.
+FOUNDATION_EXPORT const unsigned char NSDictionaryObjectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDictionaryObjects/PublicHeader.h>
+
+
